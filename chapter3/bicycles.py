6e7c98054de12b5de7f6ca00ded597273b05e0d1
yong <xixiaoyong@xiaomi.com>
2022-02-24 12:30:59 +0800

    this is auto commit

---
+++ chapter3/bicycles.py
@@ -0,0 +1,5 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "My first bicycle was a " + bicycles[0].title() + "."
+bicycles[0] = 'luck'
+print(message)
+print(bicycles[0])
---
+++ chapter3/reverse.py
@@ -0,0 +1,10 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse()
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
+
+motorcycles = []
+print(motorcycles[-1])
