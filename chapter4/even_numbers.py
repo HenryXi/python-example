35d7a4afab10ca86a9b4064c933b4498767b867c
yong <xixiaoyong@xiaomi.com>
2022-03-08 14:17:36 +0800

    this is auto commit

--- chapter4/even_numbers.py
+++ chapter4/even_numbers.py
@@ -1,8 +1,2 @@
 even_numbers = list(range(2, 11, 3))
 print(even_numbers)
-
-version = "V.sds"
-if version.endswith(".110816") or version.endswith(".PRELOAD") or version.endswith(".DEV") or not version.startswith("V"):
-    print("ignore")
-else:
-    print("handle")
